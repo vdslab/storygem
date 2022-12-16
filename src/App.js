ade3cf4fc646397343292499358ecf062ddbaa7a
oden <oden6680@gmail.com>
2022-12-17 00:09:04 +0900

    selectboxの追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import { useState } from "react";
-import Form from "./component/Form";
+//import Form from "./component/Form";
+import Select from 'react-select'
 import VoronoiTreeMap from "./component/VoronoiTreeMap";
-import defaultData from "./data/data-en.json";
+import defaultData from "./data/VASTData.json";
+
+import infoVisData from "./data/InfoVisData.json";
+import VASTData from "./data/VASTData.json";
+import scVisData from "./data/scVisData.json";
+
+const options = [
+  { value: 'scvis', label: 'scvis' },
+  { value: 'infovis', label: 'infovis' },
+  { value: 'vast', label: 'VAST' },
+]
+
+const dataObj = {
+  "scvis": scVisData,
+  "infovis": infoVisData,
+  "vast": VASTData
+}
 
 const App = () => {
   const [data, setData] = useState(defaultData);
+  const onChange = e => {
+    setData(dataObj[e.value]);
+  }
   return (
     <div className="App">
-      <Form setData={setData} />
+      {/*<Form setData={setData} />*/}
+      <Select options={options} onChange={onChange}></Select>
       <VoronoiTreeMap data={data} />
     </div>
   );
