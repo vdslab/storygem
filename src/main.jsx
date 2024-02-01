91f73e136404cacac37d462c52ee6ac7514547e7
Yosuke Onoue <onoue@likr-lab.com>
2024-02-02 02:14:51 +0900

    フォントが読み込まれていない問題を修正

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,26 @@ import App from "./App";
 import { fonts, fontSize } from "./fonts";
 
 const link = document.createElement("link");
-const params = new URLSearchParams();
+const params = [];
 for (const font of fonts) {
-  params.append("family", font.query);
+  if (font.query) {
+    params.push(`family=${font.query}`);
+  }
 }
-params.append("display", "swap");
-link.href = `https://fonts.googleapis.com/css2?${params}`;
+params.push("display=swap");
+link.href = `https://fonts.googleapis.com/css2?${params.join("&")}`;
 link.rel = "stylesheet";
 document.head.append(link);
 
-Promise.all(
-  fonts.map((font) => document.fonts.load(`${fontSize}px ${font.name}`)),
-).then(() => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-});
+setTimeout(() => {
+  Promise.all(
+    fonts.map((font) => document.fonts.load(`${fontSize}px ${font.name}`)),
+  ).then(() => {
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  });
+}, 0);
